5e5c38b8ef860d03d0ae87c3bf35fc5ae30b3d57
乄腾龙丶 <380946176@qq.com>
2017-09-21 15:00:54 +0800

    delete spring-security

--- spring-security/src/main/java/com/didispace/MyInvocationSecurityMetadataSourceService.java
+++
@@ -1,7 +0,0 @@
-package com.didispace;
-
-import org.springframework.security.web.access.intercept.FilterInvocationSecurityMetadataSource;
-
-public class MyInvocationSecurityMetadataSourceService implements FilterInvocationSecurityMetadataSource {
-
-}
--- spring-security/src/main/java/com/didispace/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.didispace.repository;
-
-import com.didispace.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-	User findByUsername(String username);
-}
